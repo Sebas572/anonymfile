c152cfd13800bafe9f87b10752e3b3f474f27139
Sebastian <sebastian.balebona@gmail.com>
2022-08-26 18:24:53 -0400

    Fix bugs

--- CompressFiles.js
+++ CompressFiles.js
@@ -11,55 +11,70 @@ const formats = {
 }
 
 const CompressFiles = ({directory, compress}) => {
-	let complete;
-
-	let FindError;
-	directory.forEach((dir) => {
-		if(!fs.existsSync(dir)) {
-			FindError = {Error: 'This file does not exist'};
-		}
-	})
-
-	if(FindError !== undefined) return FindError;
-
-	const type = formats[compress.type];
-	const fileName = path.join(__dirname, '.tmp', `${compress.name}.${type}`);
-	const output = fs.createWriteStream(fileName);
-	const archive = archiver(compress.type, {zlib: { level: 9 }});
-
-	output.on('end', () => { console.error('Data has been drained'); })
-
-	archive.on('warning', (err) => {
-		if (err.code === 'ENOENT') console.warn('log warning');
-		else {
-			console.error(err);
-			return {Error: 'Wanrning in compress files'}
-		};
-	});
-
-	archive.on('error', (err) => {
-		console.error(err);
-		if(err) return {Error: 'Error in compress files'};
-	});
-
-	archive.pipe(output);
-	directory.forEach(element_directory => {
-		const name = path.basename(element_directory);
-		archive.append(fs.createReadStream(element_directory), { name: name });
-	})
-
-	output.on('close', () => {
-		complete(fileName);
-	})
-	archive.finalize();
-
 	return new Promise((resolve, reject) => {
-		complete = resolve;
+		directory.forEach((dir) => {
+			if(!fs.existsSync(dir)) {
+				const err = `This file does not exist \"${dir}\"`;
+				
+				reject({Error: err});
+				throw(err);
+			}
+		})
+
+		const type = formats[compress.type];
+		const fileName = path.join(__dirname, '.tmp', `${compress.name}.${type}`);
+		const output = fs.createWriteStream(fileName);
+		const archive = archiver(compress.type, {zlib: { level: 9 }});
+
+		output.on('end', () => {
+			const message = 'Data has been drained';
+			console.error(message);
+			reject({Error: message});
+		});
+
+		archive.on('warning', (err) => {
+			if (err.code === 'ENOENT') console.warn('log warning');
+			else {
+				console.error(err);
+				reject({Error: 'Wanrning in compress files'});
+			};
+		});
+
+		archive.on('error', (err) => {
+			if(err) {
+				console.error(err);
+				reject({Error: 'Error in compress files'})
+			};
+		});
+
+		archive.pipe(output);
+		directory.forEach(element_directory => {
+			const name = path.basename(element_directory);
+			archive.append(fs.createReadStream(element_directory), { name: name });
+		})
+
+		output.on('close', () => {
+			resolve(fileName);
+		})
+		archive.finalize();
 	})
 }
 
-const compress = async({directory, compress}) => {
-	if(!compress.active) return directory;
+const startCompression = async({directory, compress}) => {
+	try {
+		const data = await CompressFiles({directory: directory, compress: compress});
+		return data;
+	}catch (err) {
+		return err;
+	};	
+}
+
+const compressFiles = async({directory, compress}) => {
+	if(compress.active === undefined) return directory;
+	if(typeof compress.active !== 'boolean') return {Error: 'The variable \"compress.active\" must be of type \"Boolean\"'};
+	if(typeof compress.name !== 'string') return {Error: 'The variable \"compress.name\" must be of type \"String\"'};
+	if(typeof compress.type !== 'string') return {Error: 'The variable \"compress.type\" must be of type \"String\" and the available options are \"tar\" or \"zip\"'};
+	if(!compress.active === true) return directory;
 	
 	if(!Array.isArray(directory)) directory = [directory];
 	let FileCompress;
@@ -67,9 +82,9 @@ const compress = async({directory, compress}) => {
 	compress.type = compress.type.toLowerCase();
 
 	if(compress.type === 'tar') {
-		return await CompressFiles({directory: directory, compress: compress});
+		return await startCompression({directory: directory, compress: compress});
 	}else if(compress.type === 'zip') {
-		return await CompressFiles({directory: directory, compress: compress});
+		return  await startCompression({directory: directory, compress: compress});
 	}else {
 		const message = 'Accepted compression format types are \"tar.gz\" and \"zip\"';
 		console.error(message);
@@ -77,4 +92,4 @@ const compress = async({directory, compress}) => {
 	}
 }
 
-module.exports = compress;
+module.exports = compressFiles;
--- index.js
+++ index.js
@@ -8,40 +8,48 @@ const compressFiles = require('./CompressFiles.js');
 const url = 'https://anonymfile.com/api/v1/upload';
 
 const upload = async(directory, compress) => {
-	const form = new FormData();
+	return new Promise(async(resolve, reject) => {
+		const form = new FormData();
+		
+		if(!fs.existsSync(directory)) {
+			reject({Error: `This file does not exist \"${directory}\"`});
+		}else {
+			form.append('file', fs.createReadStream(directory));
 
-	
-	if(!fs.existsSync(directory)) return {Error: 'This file does not exist'};
-	form.append('file', fs.createReadStream(directory));
-
-	const response = await axios.post(url, form, {
-		'maxContentLength': Infinity,
-		'maxBodyLength': Infinity
-	}).catch(err => {
-		const reason = 'An error occurred while trying to connect';
-
-		throw (reason);
-	}).finally(() => {
-		//remove compression that was previously saved
-		if(compress.active) {
-			fs.rm(directory, (err) => {
-				if(err) console.log('Error in remove file .tmp');
+			const response = await axios.post(url, form, {
+				'maxContentLength': Infinity,
+				'maxBodyLength': Infinity
+			}).catch(err => {
+				reject({Error: 'An error occurred while trying to connect'});
+			}).finally(() => {
+				//remove compression that was previously saved
+				if(compress.active) {
+					fs.rm(directory, (err) => {
+						if(err) console.log('Error in remove file .tmp');
+					});
+				}
 			});
+
+			const Json = response.data;
+			Json['url'] = Json.data.file.url.full;
+			
+			resolve(Json);
 		}
 	});
-	const Json = response.data;
-	Json['url'] = Json.data.file.url.full;
-	
-	return Json;
 }
 
 const multiple_uploads = (directory, compress) => {
 	const Json = [];
 
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		directory.forEach(async(element, index) => {
-			const get = await upload(element, compress);
-			Json.push(get);
+			try {
+				const get = await upload(element, compress);
+				Json.push(get);
+			
+			}catch (err) {
+				Json.push(err);
+			}
 			
 			if(Json.length === directory.length) resolve(Json);
 		});
@@ -51,6 +59,8 @@ const multiple_uploads = (directory, compress) => {
 
 //you must send the directory of the file to the "AnonymousFiles" function
 const AnonymousFiles = async({directory, compress={active: false, name: 'Archive', type: 'tar'}}) => {
+	if(directory === undefined) return {Error: 'You must pass the directory of the file'}
+
 	if(compress.active === undefined) compress.active = false;
 	if(compress.name === undefined) compress.name = 'Archive';
 	if(compress.type === undefined) compress.type = 'tar';
@@ -58,7 +68,14 @@ const AnonymousFiles = async({directory, compress={active: false, name: 'Archive
 	directory = await compressFiles({directory, compress});
 	if(directory.Error !== undefined) return directory;
 
-	if(!Array.isArray(directory)) return await upload(directory, compress);
+	if(!Array.isArray(directory)) {
+		try {
+			const data = await upload(directory, compress);
+			return data;
+		}catch(err) {
+			return err;
+		}
+	}
 	else return await multiple_uploads(directory, compress);
 
 }
